d5ff47f08ab3fd07f4cdfc7d1334e38544fcdd02
Austin Cary <acary@gatessolutions.com>
2019-08-15 16:50:32 -0500

    using list component, listening for tab event. More styling and use of material icons.

--- js/app.js
+++ js/app.js
@@ -2,9 +2,27 @@ import { MDCTabBar } from '@material/tab-bar';
 import { MDCTopAppBar } from '@material/top-app-bar';
 
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+tabBar.activateTab(0);
+document
+  .querySelector('.mdc-tab-bar')
+  .addEventListener('MDCTabBar:activated', event => {
+    showTab(event.detail.index);
+  });
 
 // Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 
 // document.querySelector('.mdc-top-app-bar__title').innerHTML = 'Respiratory Discstress';
+
+function showTab(index) {
+  var scheduleTable = document.querySelector('.mdc-data-table');
+  var nextGame = document.getElementById('next-game');
+  if (index === 0) {
+    scheduleTable.style.display = 'block';
+    nextGame.style.display = 'none';
+  } else {
+    scheduleTable.style.display = 'none';
+    nextGame.style.display = 'block';
+  }
+}
