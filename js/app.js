4378bbb408c6d4cd7aafa50e15e6f6fae675ae1b
Austin Cary <acary@gatessolutions.com>
2019-08-19 16:30:21 -0500

    remove drawer aside which doesn't work with es6 modules. Add Data list of scheduling cards as a conversion of the data table at small screen sizes. Adjust team/player info responsiveness

--- js/app.js
+++ js/app.js
@@ -1,19 +1,4 @@
-import { MDCDrawer } from '@material/drawer';
 import { MDCTabBar } from '@material/tab-bar';
-import { MDCTopAppBar } from '@material/top-app-bar';
-
-// import { MDCList } from '@material/list';
-// const list = MDCList.attachTo(document.querySelector('.mdc-list'));
-// list.wrapFocus = true;
-
-const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-
-const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
-topAppBar.setScrollTarget(document.getElementById('players'));
-topAppBar.listen('MDCTopAppBar:nav', () => {
-  console.log('clicked');
-  // drawer.open = !drawer.open;
-});
 
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 tabBar.activateTab(0);
