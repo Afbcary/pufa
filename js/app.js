362918e4b7b349c210af42ad958a485eb691fe45
Austin Cary <acary@gatessolutions.com>
2019-08-19 09:10:29 -0500

    adding drawer aside to toggle from top app bar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
+import { MDCDrawer } from '@material/drawer';
 import { MDCTabBar } from '@material/tab-bar';
 import { MDCTopAppBar } from '@material/top-app-bar';
+
+// import { MDCList } from '@material/list';
+// const list = MDCList.attachTo(document.querySelector('.mdc-list'));
+// list.wrapFocus = true;
+
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+
+const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+topAppBar.setScrollTarget(document.getElementById('players'));
+topAppBar.listen('MDCTopAppBar:nav', () => {
+  console.log('clicked');
+  // drawer.open = !drawer.open;
+});
 
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 tabBar.activateTab(0);
@@ -8,12 +22,6 @@ document
   .addEventListener('MDCTabBar:activated', event => {
     showTab(event.detail.index);
   });
-
-// Instantiation
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
-
-// document.querySelector('.mdc-top-app-bar__title').innerHTML = 'Respiratory Discstress';
 
 function showTab(index) {
   var scheduleTable = document.querySelector('.mdc-data-table');
